9ed0e3432815e2d2eba5a742c31637afbeff6b6d
Adrian Gidaro <adriangidaro@gmail.com>
2024-12-02 21:01:25 +1100

    End of lesson

---
+++ color.py
@@ -0,0 +1,5 @@
+from color50 import rgb, constants
+
+my_color = rgb(128, 0, 128)
+print(constants.BRIGHT_BLUE + "Hello, World!" + constants.RESET)
+print(my_color + 'Test' + constants.RESET)
---
+++ packages.py
@@ -0,0 +1,7 @@
+# Packages
+
+# python3 -m venv venv - in terminal - creates a hidden virtual environment folder venv to house package control
+# ensure interpreter path is set to the local version within venv folder
+
+# pip freeze - shows packages
+# pip freeze > requirements.txt - creates text file with requirements
