bf7c5578e44ca028ff426ea9326453019c7894af
Adrian Gidaro <adriangidaro@gmail.com>
2024-12-02 20:02:44 +1100

    Mid lesson

---
+++ main.py
@@ -0,0 +1,13 @@
+# from my_module import person, foo
+
+# import my_module
+
+# foo(person)
+
+# from my_module import greeting
+
+# print(greeting)
+
+import modules.my_module as spam
+
+print(dir(spam))
